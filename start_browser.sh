cad2ef1a3331fc6d5b619a86437109df7abdd166
AbdulQadir98 <62184074+AbdulQadir98@users.noreply.github.com>
2024-08-26 09:15:56 +0530

    init start up script

--- start_browser.sh
+++ start_browser.sh
@@ -1,23 +1,19 @@
 #!/bin/bash
-
 echo "Starting script at $(date)" >> /home/pearl/start.log
-
 # Wait for the system to fully boot
-sleep 20
-
+sleep 10
 echo "wating for X server to start..." >> /home/pearl/start.log
 
 # Wait until X server is ready
 while ! xset q &>/dev/null; do
-    echo "server not ready yet, put to sleep" >> /home/pearl/start.log
+    echo "X server not ready yet, put to sleep..." >> /home/pearl/start.log
     sleep 5
 done
-
 echo "X server is ready. Launching Midori..." >> /home/pearl/start.log
+
 # Set DISPLAY explicitly
 export DISPLAY=:0
-
-# Launch midori in full-screen mode
-midori -e Fullscreen -a file:///home/pearl/index.html
+# Launch app using midori in full-screen mode
+midori -e Fullscreen -a http://192.168.8.189:3000
 
 echo "Finished script at $(date)" >> /home/pearl/start.log
